ba00ae4838ed11a31431c976920d4a5fe02c4442
Daniel Mateos <daniel@mateos.io>
2024-02-20 13:03:29 +0100

    Modify for Seeed Xiao RP2040

--- blink/button_blink/button_blink.ino
+++ blink/button_blink/button_blink.ino
@@ -3,16 +3,18 @@ int reading_a, reading_b;
 bool state;
 int button;
 
-int TEMPERATURE_SENSOR_A = 8;
-int TEMPERATURE_SENSOR_B = 9;
-int LED = 48;
-int BUTTON = 0;
+// Barduino. Button pressed is 0
+// int LED = 48; 
+// int BUTTON = 0;
+
+// Seeed XIAO RP2040. Button pressed is 1
+int LED = 26; 
+int BUTTON = 27;
+
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(TEMPERATURE_SENSOR_A, INPUT);
-  pinMode(TEMPERATURE_SENSOR_B, INPUT);
   pinMode(BUTTON, INPUT);
 
   pinMode(LED, OUTPUT);
@@ -23,14 +25,12 @@ void setup() {
 
 void loop() {
 
-  reading_a = analogRead(TEMPERATURE_SENSOR_A);  // no idea how this is used.
-  reading_b = analogRead(TEMPERATURE_SENSOR_B);  // no idea how this is used.
   button = digitalRead(BUTTON); // 1 is unpressed
 
   Serial.println(button);
 
   // Blink if pressed, non blocking
-  if (millis() - start >= 50 && !button) {
+  if (millis() - start >= 50 && button) {
     state = !state;
     digitalWrite(LED, state);
     start = millis();
