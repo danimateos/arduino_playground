9aa554508a46f245dc7b5c76200260d9341453f3
Daniel Mateos <daniel@mateos.io>
2024-03-28 19:08:18 +0100

    Control row-column matrix multiplexing with timers
    * Outstanding issue: incosistent behavior above 20Hz or so, so current implementation is useless.

--- micropython/multiplexing.py
+++ micropython/multiplexing.py
@@ -1,10 +1,14 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, Timer, UART
 import time
 
+import micropython
+micropython.alloc_emergency_exception_buf(100)
 
+
+## Barduino
 r1 = Pin(12, Pin.OUT)
 r2 = Pin(13, Pin.OUT)
-r3 = Pin(14, Pin.OUT)
+r3 = Pin(11, Pin.OUT)  # Barduino 14 is connected to the buzzer
 r4 = Pin(15, Pin.OUT)
 
 c1 = Pin(16, Pin.OUT)
@@ -41,41 +45,55 @@ def rows_off():
         row.off()
         
         
-def row_pattern(row, pattern, duration):
+def row_pattern(rows, pattern, duration):
     
+    Timer(1).deinit()
     all_off()
-    row.on()
+
+    this_row = rows[0]
+    other_rows = rows[1:]
+    these_lights = pattern[:4]
+    other_lights = pattern[4:]
     
-    for on, col in zip(pattern, cols):
+    this_row.on()
+    for on, col in zip(these_lights, cols):
         if on:
             col.on()
         
+        
+    def next_step(t):
+        
+        all_off()
+        print(other_lights)
+        if len(other_rows) > 0:
+            row_pattern(other_rows, other_lights, duration)
     
-    time.sleep(duration)
-    rows_off()
-    cols_off()
-    
-    
-def pattern(pattern, duration, refresh_rate=60):
-
-    for frame in range(duration * refresh_rate):
-    
-        for row_p, row in zip(pattern, rows):
-            row_pattern(row, row_p, (1 / refresh_rate) / 4)  # 4 roqs
+    timer = Timer(1).init(period=duration, mode=Timer.ONE_SHOT, callback=lambda t: next_step(t))        
     
     
+def show(pattern, duration, refresh_rate):
+    
+    millis = 1000 // refresh_rate
+    
+    Timer(0).init(freq=refresh_rate, mode=Timer.PERIODIC, callback = lambda t: row_pattern(rows, pattern, millis // 4)) # shortcut; might lead to inaccuracy
+    
+    Timer(2).init(period=duration, mode=Timer.ONE_SHOT, callback = lambda t: Timer(0).deinit())
+                  
+                  
+                  
+                  
 def pos_to_row_col(n):
     
     row = rows[n % 4]
     col = cols[n // 4]
     
 
-primes = [[True, True, True, False],
-          [True, False, True, False],
-          [False, False, True, False],
-          [True, False, False, False]]
+# 1 to 16
+primes = (True, True, True, False, True, False, True, False,
+          False, False, True, False, True, False, False, False)
 
 
+show(primes, 2000, refresh_rate = 10)
     
     
     
